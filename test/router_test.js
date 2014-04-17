6fe5d40f0f738950d849de3b57d819e1d282ea8b
macournoyer <macournoyer@gmail.com>
2014-04-16 22:38:00 -0400

    Fix tests.

--- test/router_test.js
+++ test/router_test.js
@@ -1,47 +1,22 @@
 var assert = require('assert'),
-    Router = require('../lib/router')
+    greatRouter = require('../');
 
 describe('Router', function() {
-  it('build routes tree', function() {
-    var router = new Router()
+  it('call routes', function() {
+    var router = greatRouter(),
+        called;
 
-    router.route('GET', '/', 'root callback')
-    router.route('GET', '/users/new', 'new callback')
-    router.route('GET', '/users/login', 'login callback')
+    router.get('/', function() { called = '/' });
+    router.get('/users/new', function() { called = 'new' });
+    router.get('/users/login', function() { called = 'login' });
 
-    // console.log(pp(router.routes))
+    router.handle({ method: 'GET', path: '/' }, function() {});
+    assert.equal(called, '/');
 
-    var expectedRoutes = {
-      method: 'GET',
-      path: '/',
-      callbacks: ['root callback'],
-      routes: {
-        '/users':  {
-          method: 'GET',
-          path: '/users',
-          callbacks: [],
-          routes: {
-            '/new': {
-              method: 'GET',
-              path: '/users/new',
-              callbacks: ['new callback'],
-              routes: {}
-            },
-            '/login': {
-              method: 'GET',
-              path: '/users/login',
-              callbacks: ['login callback'],
-              routes: {}
-            }
-          }
-        }
-      }
-    }
+    router.handle({ method: 'GET', path: '/users/new' }, function() {});
+    assert.equal(called, 'new');
 
-    assert.equal(pp(router.routes.GET), pp(expectedRoutes))
-  })
-
-  function pp(object) {
-    return JSON.stringify(object, null, 4)
-  }
-})
+    router.handle({ method: 'GET', path: '/users/login' }, function() {});
+    assert.equal(called, 'login');
+  });
+});
