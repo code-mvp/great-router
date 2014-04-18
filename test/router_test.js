de72c2a62de3eb17cd0ef6e64f221d66052d6f3c
macournoyer <macournoyer@gmail.com>
2014-04-17 22:39:57 -0400

    Add missing res arg in router.handle in tests.

--- test/router_test.js
+++ test/router_test.js
@@ -10,13 +10,13 @@ describe('Router', function() {
     router.get('/users/new', function() { called = 'new' });
     router.get('/users/login', function() { called = 'login' });
 
-    router.handle({ method: 'GET', path: '/' }, function() {});
+    router.handle({ method: 'GET', path: '/' }, {}, function() {});
     assert.equal(called, '/');
 
-    router.handle({ method: 'GET', path: '/users/new' }, function() {});
+    router.handle({ method: 'GET', path: '/users/new' }, {}, function() {});
     assert.equal(called, 'new');
 
-    router.handle({ method: 'GET', path: '/users/login' }, function() {});
+    router.handle({ method: 'GET', path: '/users/login' }, {}, function() {});
     assert.equal(called, 'login');
   });
 });
