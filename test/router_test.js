76fee1524040a9e6ad3fd887e376821a41d395fc
macournoyer <macournoyer@gmail.com>
2014-04-17 22:53:35 -0400

    Optimize Route.prototype.getCallbacks a bit
    Store method in uppercase since that's how Node server returns it.
    Replace Array.isArray w/ simpler operator.
    Rename stack to callbacks for clarity.

--- test/router_test.js
+++ test/router_test.js
@@ -4,18 +4,21 @@ var assert = require('assert'),
 describe('Router', function() {
   it('call routes', function() {
     var router = greatRouter(),
-        called;
+        called = '';
 
-    router.get('/', function() { called = '/' });
-    router.get('/users/new', function() { called = 'new' });
-    router.get('/users/login', function() { called = 'login' });
+    router.get('/', function() { called += '/' });
+    router.get('/users/new', function() { called += 'new' });
+    router.get('/users/login', function() { called += 'login' });
 
+    called = ''
     router.handle({ method: 'GET', path: '/' }, {}, function() {});
     assert.equal(called, '/');
 
+    called = ''
     router.handle({ method: 'GET', path: '/users/new' }, {}, function() {});
     assert.equal(called, 'new');
 
+    called = ''
     router.handle({ method: 'GET', path: '/users/login' }, {}, function() {});
     assert.equal(called, 'login');
   });
